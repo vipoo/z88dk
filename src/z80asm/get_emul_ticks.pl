6499cfdf3f7acde007771d02f8d67aeedf1b4f72
Paulo Custodio <pauloscustodio@gmail.com>
2019-12-24 16:29:18 +0000

    Allow a START marker to measure ticks after the setup code, ignore assembly errors if code not compatible with processor.

--- src/z80asm/get_emul_ticks.pl
+++ src/z80asm/get_emul_ticks.pl
@@ -18,20 +18,26 @@ for my $cpu (qw( 8080 8085 gbz80 r2k z180 z80 z80n )) {
 	for my $carry (0..1) {
 		path("test.asm")->spew(
 			($carry ? "scf\n" : "and a\n"),
+			";START\n",
 			$asm,
-			"nop ; END\n");
-		run("z80asm -b -l -m$cpu test.asm");
-		# get end address
-		my $end;
-		for (path("test.lis")->lines) {
-			if (/^\d+\s+([0-9A-F]{4})\s+00\s+nop ; END/) { $end = $1; last; }
-		}
-		defined($end) or die "end address not found\n";
-		# end=1: skip and a/scf in ticks count
-		run("z88dk-ticks -m$cpu -start 0001 -end $end test.bin >test.out");	
-		my $t = path("test.out")->slurp;
-		$t =~ /^(\d+)\s*$/ or die "expected ticks count, got $t\n";
-		push @T, 0+$1;
+			"\n",
+            "nop;END\n");
+        eval {
+            run("z80asm -b -l -m$cpu test.asm");
+            # get end address
+            my($start, $end);
+            for (path("test.lis")->lines) {
+                if (/^\d+\s+([0-9A-F]{4})\s+;START/) { $start = $1; }
+                if (/^\d+\s+([0-9A-F]{4})\s+00\s+nop;END/) { $end = $1; }
+            }
+            defined($start) or die "start address not found\n";
+            defined($end) or die "end address not found\n";
+
+            run("z88dk-ticks -m$cpu -start $start -end $end test.bin >test.out");	
+            my $t = path("test.out")->slurp;
+            $t =~ /^(\d+)\s*$/ or die "expected ticks count, got $t\n";
+            push @T, 0+$1;
+        };
 	}
 	@T = sort {$a <=> $b} @T;
 	$tb->add(";", $cpu, @T);
